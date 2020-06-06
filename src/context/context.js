3e5314bd23cbe6995552582d335b12def67b6017
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-06-06 09:46:34 +0200

    added Context

---
+++ src/context/context.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Context = React.createContext([{}, () => {}]);
+
+const ContextProvider = (props) => {
+    const [state, setState] = useState({});
+    return (
+        <Context.Provider value={[state, setState]}>
+            {props.children}
+        </Context.Provider>
+    );
+}
+
+export { Context, ContextProvider };
