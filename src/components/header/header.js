458dab6aa373fe1b9a592926ee884aafbe01f80d
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-06-06 11:19:47 +0200

    added header

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './headerStyle'
+
+export default () => {
+    return (
+        <Header>
+            
+        </Header>
+    )
+}
---
+++ src/components/header/headerStyle.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export default styled.header`
+background: ${props => props.theme.colors.blue};
+height: 15em; 
+`
