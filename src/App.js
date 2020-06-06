06f25bb50ce63941cd81a81ec6bcece1bc182c8a
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-06-06 10:37:38 +0200

    added router in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import './App.css'
-import {ContextProvider} from './context/context'
+import { ContextProvider } from './context/context'
 import Theme from './theme/theme'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Test from './components/test'
+import Header from './components/header/header'
 
 function App() {
-
 
   return (
     <ContextProvider>
       <Theme>
-        
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <p>About</p>
+            </Route>
+            <Route path="/">
+              <p>Home</p>
+            </Route>
+          </Switch>
+        </Router>
       </Theme>
     </ContextProvider>
   );
