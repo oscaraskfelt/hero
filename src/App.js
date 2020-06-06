39a61b3db6988c84102974136b343081dec83f6f
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-06-06 10:15:03 +0200

    added Theme to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import {ContextProvider} from './context/context'
+import Theme from './theme/theme'
+import Test from './components/test'
 
 function App() {
 
 
   return (
     <ContextProvider>
-      <div className="App">
-      hej
-      </div>
+      <Theme>
+        
+      </Theme>
     </ContextProvider>
   );
 }
 
-export default App;
+export default App
