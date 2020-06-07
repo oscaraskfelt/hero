64d1ea060753c333df3b782113f819b86664f09b
Oscar Askfelt <oscaraskfelt@gmail.com>
2020-06-07 14:40:12 +0200

    shuffling array

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -3,11 +3,21 @@ import Card from '../card/card'
 import { Board } from './boardStyle'
 
 export default () => {
+ 
+    const cards = [{id: 1}, {id: 2}, {id: 3}]
+    cards.push(...cards)
+    for(let i = cards.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = cards[i]
+        cards[i] = cards[j]
+        cards[j] = temp
+      }
+
     return (
         <Board>
-            {[...Array(6)].map((el, i) => {
+            {cards.map((item, i) => {
                 return(
-                    <Card key={i}/>
+                    <Card item={item.id} key={i}/>
                 )}
             )}
         </Board>
